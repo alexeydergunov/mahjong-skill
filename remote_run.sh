5a043da74ed70730262be2ffa786841078804c3c
Alexey Dergunov <alexey.dergunov@gmail.com>
2024-12-13 18:54:00 +0300

    Script for running docker image on pantheon machine

---
+++ remote_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+docker run \
+  -e MIMIR_USER=mimir \
+  -e MIMIR_PASSWORD=pgpass \
+  -e MIMIR_DB_NAME=mimir \
+  -e MIMIR_HOST=db.pantheon.internal \
+  -e MIMIR_PORT=5432 \
+  -e FREY_USER=frey \
+  -e FREY_PASSWORD=pgpass \
+  -e FREY_DB_NAME=frey \
+  -e FREY_HOST=db.pantheon.internal \
+  -e FREY_PORT=5432 \
+  --network=host \
+  --volume ./docker-out/:/work/out/ \
+  mahjong-skill
+
+echo "Docker run completed"
+
+ls -la ./docker-out/
