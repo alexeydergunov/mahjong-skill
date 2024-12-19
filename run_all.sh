41226dc5332f41d15a02a9bded11fdee165d5bf5
Alexey Dergunov <alexey.dergunov@gmail.com>
2024-12-19 23:56:36 +0300

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-./main.py elo >_elo.txt
-./main.py trueskill >_ts.txt
-./main.py openskill_pl >_os_pl.txt
-./main.py openskill_bt >_os_bt.txt
+#./main.py --load-from-portal --model elo > _elo.txt
+./main.py --load-from-portal --model trueskill > _ts.txt
+./main.py --load-from-portal --model openskill_pl > _os_pl.txt
+./main.py --load-from-portal --model openskill_bt > _os_bt.txt
 
 echo "All done"
